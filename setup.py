e395725e7618710e4514cfec2a00e767c2a6d0ea
Aman23051999 <singhaman23051999@gmail.com>
2025-09-15 10:15:43 +0530

    folder Structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aman Singh',
+    author_email= 'singhaman23051999@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
